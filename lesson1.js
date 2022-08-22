d9cd94c3f62480c35788e550ebe5dc6cd82eebb8
Eleonora Pogorelova <e.i.pogorelova@tcsbank.ru>
2022-08-22 16:53:48 +0300

    new commit

---
+++ lesson1.js
@@ -0,0 +1,27 @@
+// Задание 1
+let readline = require( 'readline-sync' );
+const tcTemp = readline.question( 'Set temperature in degrees Celsius:  \n ' );
+const tfTemp = ( 9 / 5 ) * +tcTemp + 32;
+console.log( tfTemp );
+
+
+
+// Задание 2 
+let firstName;
+let admin;
+firstName = "Василий";
+admin = firstName;
+console.log( admin ); //Василий
+
+
+
+// Задание 3
+console.log( 1000 + "108" ); //Ответ 1000108
+
+
+
+
+// Задание 4
+// Самостоятельно разобраться с атрибутами тега script( async и defer ).
+
+
